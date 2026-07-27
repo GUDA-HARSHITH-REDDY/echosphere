24fbab4d6f41f2866c9f49946f41b9d148437043
Guda Harshith Reddy <harshithguda265@gmail.com>
2026-07-27 14:27:30 +0530

    my first commit

---
+++ prisma/migrations/20260727084746_add_profile_image/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "RecyclingCenter" ADD COLUMN     "city" TEXT;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "profileImageUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "WasteReport" ADD COLUMN     "city" TEXT;
